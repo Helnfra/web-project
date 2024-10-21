76166877e3b20af08fcc668a687e21c9b9129b34
Helnfra <ogenwamma@gmail.com>
2024-10-21 08:26:05 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "60%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
